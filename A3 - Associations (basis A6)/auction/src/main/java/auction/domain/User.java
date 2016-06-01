abfc95cffe13472e4c8085a4431613fe85777d19
nino-vrijman <n.vrijman@student.fontys.nl>
2016-06-01 20:22:57 +0200

    Re-organised assignments

---
+++ A3 - Associations (basis A6)/auction/src/main/java/auction/domain/User.java
@@ -0,0 +1,27 @@
+package auction.domain;
+
+import javax.persistence.*;
+
+@Entity @Table(name="USER")
+@NamedQueries({
+        @NamedQuery(name = "User.findByEmail", query = "select u from User as u where u.email = :email"),
+        @NamedQuery(name = "User.count", query = "select count(u) from User as u")
+})
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String email) {
+        this.email = email;
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
